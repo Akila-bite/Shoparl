57ce222fd4ec0b533601937e271b53a98c74575d
Nompumelelo <akilaprofvictoria@gmail.com>
2024-11-25 06:23:00 +0200

    added images and prices in the notebooks page, added a search bar and icon in the nav

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+// scipt for the available product links in the shopping page
+document.getElementById("product-link").addEventListener("click", function() {
+    window.open("https://example.com", "_blank"); // Opens in a new tab
